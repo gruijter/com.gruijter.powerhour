92e49e62fdd66cb86d738aceab69f58c078b6ce3
gruijter <gruijter@hotmail.com>
2023-04-27 12:37:48 +0200

    v5.5.3 Battery: Improved hedge strategy.

--- hedge_strategy.js
+++ hedge_strategy.js
@@ -23,7 +23,7 @@ along with com.gruijter.powerhour.  If not, see <http://www.gnu.org/licenses/>.
 const getStrategy = ({
 	prices,	// array of hourly prices, e.g. [0.331, 0.32, 0.322, 0.32, 0.328, 0.339, 0.429, 0.331, 0.32, 0.322, 0.32, 0.328, 0.339, 0.429];
 	minPriceDelta = 0.1,	// mimimum price difference from highest or lowest price to sell/buy
-	soc = 0,	// Battery State of Charge at start of first hour
+	soc = 0,	// Battery State of Charge at start of first hour in %
 	batCapacity = 5.05, // in kWh
 	chargePower = 2000, // in Watt
 	dischargePower = 1700, // in Watt
@@ -39,12 +39,12 @@ const getStrategy = ({
 		.reverse()
 		.slice(0, Math.ceil(prcs.length / 3))
 		.reverse();
-	// limit search to the first 10 peaks/troughs that have min delta
+	// limit search to the first 10 peaks/troughs that have minimum delta
 	const peakPrices = prcs
 		.filter((price, idx) => {
 			const futureMin = Math.min(...[...prcs].slice(idx));
 			const futureMax = Math.max(...[...prcs].slice(idx));
-			return (price >= peaks[0] && (price - futureMin) > minPriceDelta * 0.7)
+			return (price >= peaks[0] && (price - futureMin) > minPriceDelta * 0.7) // promiscuous selling
 			|| (price <= troughs[0] && (futureMax - price) > minPriceDelta);
 		})
 		.slice(0, 10);
@@ -54,10 +54,9 @@ const getStrategy = ({
 	const chargeSpeed = chargePower / (batCapacity * 10); // % per hour
 	const dischargeSpeed = dischargePower / (batCapacity * 10); // % per hour
 	const batCapPerc = batCapacity / 100;
-	// calculate average prices
 	const avgPrice = prcs.reduce((a, b) => (a + b)) / prcs.length;
-	const avgPeakPrice = peakPrices.reduce((a, b) => (a + b)) / peakPrices.length;
-	// console.log(prcs, peaks, troughs, peakPrices, avgPeakPrice);
+	// const avgPeakPrice = peaks.reduce((a, b) => (a + b)) / peaks.length;
+	// console.log(prcs, peaks, troughs, peakPrices, avgPeakPrice, avgPrice);
 	const startState = {
 		profit: 0,
 		prices: peakPrices,
@@ -68,8 +67,8 @@ const getStrategy = ({
 	const stateAfter = (stateBefore) => {
 		const price = stateBefore.prices.shift();
 		const strat = [0]; // standard strategy = hold;
-		if (price >= avgPeakPrice) strat.push(1); // add sell strategy
-		if (price <= avgPeakPrice) strat.push(-1); // add buy strategy
+		if (price >= peaks[0]) strat.push(1); // add sell strategy
+		if (price <= troughs[0]) strat.push(-1); // add buy strategy
 		// run all 3 strategies
 		strat.forEach((strategy) => {
 			if (price === undefined) return null;
@@ -108,13 +107,15 @@ const getStrategy = ({
 	// console.dir(sorted, { depth: null });
 	// console.log('tested:', sorted.length);
 	// console.log('best strat:', sorted[0]);
+	// if (sorted[0].profit < 0) return 0; // hold when negative profit
 	return sorted[0].hourlyStrat[0][prices[0]];
 };
 
 // TEST
 // const prices = [0.331, 0.32, 0.322, 0.32, 0.328, 0.339, 0.429, 0.331, 0.32, 0.322, 0.32, 0.328, 0.339, 0.429];
 // const prices = [0.16, 0.17, 0.16, 0.13, 0.12, 0.12, 0.12, 0.12, 0.11, 0.11, 0.12, 0.15, 0.16, 0.16, 0.12, 0.10, 0.07, 0.04, 0.02, 0.01, 0.01, 0.06, 0.08, 0.11, 0.16, 0.17, 0.17, 0.17, 0.16];
-// console.dir(getStrategy({ prices, soc: 100 }), { depth: null });
+// const prices = [0.29, 0.28, 0.27, 0.27, 0.27, 0.29, 0.30, 0.33, 0.32, 0.28, 0.27, 0.21, 0.22, 0.22, 0.21, 0.23, 0.25, 0.27, 0.29, 0.29, 0.30, 0.29, 0.28, 0.27];
+// console.dir(getStrategy({ prices, soc: 3 }), { depth: null });
 
 module.exports = {
 	getStrategy,
