a630fdd7fe15a01b533708ae21631681188a08aa
gruijter <gruijter@hotmail.com>
2026-02-01 12:25:59 +0100

    image.js linter fixes

--- image.js
+++ image.js
@@ -1,30 +1,31 @@
+'use strict';
+
 const { Readable } = require('stream');
-const fetch = require('node-fetch');
 
 const imageCache = [];
 
 const imageUrlToStream = async (url, stream) => {
-    // check if url is in cache
-    let cacheObject = imageCache.find(object => object.url === url);
-    if (cacheObject) {
-        const readable = Readable.from(cacheObject.buffer);
-        return readable.pipe(stream);
-    }
-
-    const res = await fetch(url, { timeout: 10000 });
-    if (!res.ok) {
-        throw new Error("Invalid Response");
-    }
-    const body = await res.arrayBuffer();
-    cacheObject = { url, buffer: Buffer.from(body) };
-    imageCache.push(cacheObject);
-    if (imageCache.length > 5) {
-        imageCache.shift();
-    }
+  // check if url is in cache
+  let cacheObject = imageCache.find((object) => object.url === url);
+  if (cacheObject) {
     const readable = Readable.from(cacheObject.buffer);
     return readable.pipe(stream);
+  }
+
+  const res = await fetch(url, { timeout: 10000 });
+  if (!res.ok) {
+    throw new Error('Invalid Response');
+  }
+  const body = await res.arrayBuffer();
+  cacheObject = { url, buffer: Buffer.from(body) };
+  imageCache.push(cacheObject);
+  if (imageCache.length > 5) {
+    imageCache.shift();
+  }
+  const readable = Readable.from(cacheObject.buffer);
+  return readable.pipe(stream);
 };
 
 module.exports = {
-    imageUrlToStream,
+  imageUrlToStream,
 };
