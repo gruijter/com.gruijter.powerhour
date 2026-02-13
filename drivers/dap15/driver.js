ab034195035158e5ef7d6ddd4051a63ad3305805
gruijter <gruijter@hotmail.com>
2026-02-13 21:42:08 +0100

    enabled stekker as price service for DAP15

--- drivers/dap15/driver.js
+++ drivers/dap15/driver.js
@@ -22,7 +22,7 @@ along with com.gruijter.powerhour.  If not, see <http://www.gnu.org/licenses/>.s
 const ENTSOE_GRUIJTER = require('../../lib/providers/EntsoeGruijter');
 const ENTSOE = require('../../lib/providers/Entsoe');
 // const NP = require('../../nordpool');
-// const STEKKER = require('../../lib/providers/Stekker');
+const STEKKER = require('../../lib/providers/Stekker');
 
 const GenericDriver = require('../../lib/generic_dap_driver');
 
@@ -65,7 +65,7 @@ class dapDriver extends GenericDriver {
     this.ds = driverSpecifics;
 
     // provide all data providers to the driver in order of presedence
-    this.ds.providers = [ENTSOE_GRUIJTER, ENTSOE]; // [ENTSOE_GRUIJTER,ENTSOE, NP, STEKKER];
+    this.ds.providers = [ENTSOE_GRUIJTER, ENTSOE, STEKKER]; // [ENTSOE_GRUIJTER,ENTSOE, NP, STEKKER];
     this.ds.biddingZones = {};
     this.ds.providers.forEach((Provider) => {
       const api = new Provider();
